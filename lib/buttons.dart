7ac6ed0111666d112333c0dfe81ce56ea72ae4ef
InfinityAkshay <78033957+InfinityAkshay@users.noreply.github.com>
2021-03-02 20:11:20 +0530

    Saving

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
 
 class DrawButton extends StatelessWidget {
-  final List<Offset> points;
+  final List<Offset> drawing;
   final Function clear;
-  DrawButton(this.points,this.clear);
+  final Function save;
+  String txt = "";
+  DrawButton(this.drawing, this.clear, this.save);
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-    tooltip: 'clear Screen',
-    backgroundColor: Colors.black,
-    child: Icon(Icons.refresh),
-    onPressed: clear
+    return Stack(children: [
+      Align(
+          alignment: Alignment.centerRight,
+          child: FloatingActionButton(
+              tooltip: 'Clear Screen',
+              backgroundColor: Colors.black,
+              child: Icon(Icons.refresh),
+              onPressed: clear)),
+      Align(
+          alignment: Alignment.bottomRight,
+          child: FloatingActionButton(
+              tooltip: 'Save',
+              backgroundColor: Colors.black,
+              child: Icon(Icons.save),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) => _buildPopupDialog(context),
+                );
+              }))
+    ]);
+  }
+
+  Widget _buildPopupDialog(BuildContext context) {
+    return new AlertDialog(
+      title: const Text('Save As'),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextField(onChanged: (str) => txt = str),
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () => Navigator.of(context).pop(),
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Cancel'),
+        ),
+        new FlatButton(
+          onPressed: () {
+            save(txt, drawing);
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Save'),
+        ),
+      ],
     );
   }
 }
